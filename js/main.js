31deed647b876c9186bcf603287a99b32ffcb867
A.Alekseev <angelpsymail@gmail.com>
2018-11-14 22:47:07 +0500

    Добавление массива экранов со ссылками на template

---
+++ js/main.js
@@ -0,0 +1,52 @@
+const SCREENS_REFS = {
+  byId: {
+    intro: {
+      id: `intro`,
+      rusName: `Интро`,
+      template: document.querySelector(`#intro`),
+    },
+    greeting: {
+      id: `greeting`,
+      rusName: `Приветствие`,
+      template: document.querySelector(`#greeting`),
+    },
+    rules: {
+      id: `rules`,
+      rusName: `Правила игры`,
+      template: document.querySelector(`#rules`),
+    },
+    game1: {
+      id: `game1`,
+      rusName: `Игровой экран с двумя изображениями`,
+      template: document.querySelector(`#game-1`),
+    },
+    game2: {
+      id: `game2`,
+      rusName: `Игровой экран с одним изображением`,
+      template: document.querySelector(`#game-2`),
+    },
+    game3: {
+      id: `game3`,
+      rusName: `Игровой экран с тремя изображениями`,
+      template: document.querySelector(`#game-3`),
+    },
+    stats: {
+      id: `stats`,
+      rusName: `Общая статистика по всем игрокам`,
+      template: document.querySelector(`#stats`),
+    },
+  },
+  allIds: [
+    `intro`,
+    `greeting`,
+    `rules`,
+    `game1`,
+    `game2`,
+    `game3`,
+    `stats`,
+  ],
+};
+
+const screens = SCREENS_REFS.allIds
+  .map((id) => SCREENS_REFS.byId[id]);
+
