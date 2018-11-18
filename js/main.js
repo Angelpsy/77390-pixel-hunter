5536636390fa5a04f34589c2e60d606b803e87ae
A.Alekseev <angelpsymail@gmail.com>
2018-11-18 16:48:22 +0500

    Вынесение шаблонов из index.html в отдельные модули

--- js/main.js
+++ js/main.js
@@ -1,67 +1,17 @@
-const ROOT_EL = document.querySelector(`#main`);
+import screens from './screens/index.js';
 
-const SCREENS_REFS = {
-  byId: {
-    intro: {
-      id: `intro`,
-      rusName: `Интро`,
-      template: document.querySelector(`#intro`),
-    },
-    greeting: {
-      id: `greeting`,
-      rusName: `Приветствие`,
-      template: document.querySelector(`#greeting`),
-    },
-    rules: {
-      id: `rules`,
-      rusName: `Правила игры`,
-      template: document.querySelector(`#rules`),
-    },
-    game1: {
-      id: `game1`,
-      rusName: `Игровой экран с двумя изображениями`,
-      template: document.querySelector(`#game-1`),
-    },
-    game2: {
-      id: `game2`,
-      rusName: `Игровой экран с одним изображением`,
-      template: document.querySelector(`#game-2`),
-    },
-    game3: {
-      id: `game3`,
-      rusName: `Игровой экран с тремя изображениями`,
-      template: document.querySelector(`#game-3`),
-    },
-    stats: {
-      id: `stats`,
-      rusName: `Общая статистика по всем игрокам`,
-      template: document.querySelector(`#stats`),
-    },
-  },
-  allIds: [
-    `intro`,
-    `greeting`,
-    `rules`,
-    `game1`,
-    `game2`,
-    `game3`,
-    `stats`,
-  ],
-};
+const ROOT_EL = document.querySelector(`#main`);
 
 const ElementsClasses = {
   ARROW_LEFT: `js-arrows-left`,
   ARROW_RIGHT: `js-arrows-right`,
 };
 
-const screens = SCREENS_REFS.allIds
-  .map((id) => SCREENS_REFS.byId[id]);
-
 let indexCurrentScreen;
 
 const showScreen = (numberScreen) => {
   ROOT_EL.innerHTML = ``;
-  ROOT_EL.appendChild(screens[numberScreen].template.content.cloneNode(true));
+  ROOT_EL.appendChild(screens[numberScreen].element.cloneNode(true));
   indexCurrentScreen = numberScreen;
 };
 
